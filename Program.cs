40ce7f484f2070275deafe1aefc5456a6cf6617a
George Hoffman <GeorgeH@magenic.com>
2019-07-16 17:57:42 -0400

    First Class exercise

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Intro;
+using System;
+
+namespace ConsoleBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+                var blog = new Blog { Url = "http://sample.com" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+            using (var db = new BloggingContext())
+            {
+                var blogs = db.Blogs;
+
+                foreach (var blog in blogs)
+                {
+                    Console.WriteLine($"Blog item found {blog.BlogId}.");
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
